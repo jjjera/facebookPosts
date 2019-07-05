4d4ae16b2f5757963f9678bbb78a9487d604f638
mallikaarjun <mallikaarjun.g@apppixel.com>
2019-07-05 12:23:57 +0530

    twitter data with node

--- src/components/Twitter.js
+++ src/components/Twitter.js
@@ -9,43 +9,39 @@ class Feed extends Component{
         img:''}
     }    
 
-    componentDidMount() {
-        console.log('getDetails called')
-        axios.get('http://localhost:8080/twitter/index.php')
+    componentDidMount(){
+        console.log('cdm called');
+        axios.get('http://localhost:8010/api/social/twitter')
         .then((response) => {
-            this.setState({bearerToken:response.data});
-            console.log('response i$',this.state.bearerToken[2].text);
-            this.setState({
-                img:response.data[0].entities.media[0].media_url_https
-            })
+            this.setState({bearerToken:response.data.result});
+            console.log('response is',this.state.bearerToken);
         })
         .catch((error) => {
             console.log('error block called',error);
         })
-        console.log('outside block called')
     }
+
+    // componentDidMount() {
+    //     console.log('getDetails called')
+    //     axios.get('http://localhost:8080/twitter/index.php')
+    //     .then((response) => {
+    //         this.setState({bearerToken:response.data});
+    //         console.log('response i$',this.state.bearerToken[2].text);
+    //         this.setState({
+    //             img:response.data[0].entities.media[0].media_url_https
+    //         })
+    //     })
+    //     .catch((error) => {
+    //         console.log('error block called',error);
+    //     })
+    //     console.log('outside block called')
+    // }
 
 
     render(){
         return(
             <div>
-                <li><img src={this.state.img} alt="Smiley face"/></li>
-                {this.state.bearerToken.map((val,key) => {
-                    return (
-                        <ul key={key}>
-                            <li><img src={this.state.img} alt="Smiley face"/></li>
-                            <li>Comment : {val.text}</li>
-                            <li>User Name : {val.user.name}</li>
-                            <li>Screen Name : {val.user.screen_name}</li>
-                            <li>Favourite count : {val.favorite_count}</li>
-                            <li>Re_Tweet count : {val.retweet_count}</li>
-                            {
-                                val.extended_entities &&
-                                <li>image2 : <img src={val.extended_entities.media.media_url_https} alt="Smiley face"/></li>
-                            }
-                        </ul>
-                    )
-                })} 
+                {console.log('check',this.state.bearerToken[0])}
             </div>
         )
     }
